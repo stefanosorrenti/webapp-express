87e34ad941cfdd47503f04ce22c64e9c9d8ca2a5
stefanosorrenti <stefanosorrenti01@gmail.com>
2026-04-20 14:21:14 +0200

    add index root

--- server.js
+++ server.js
@@ -25,6 +25,28 @@ app.get('/movies', (req, res) => {
     })
 });
 
+//SHOW
+app.get('/movies/:id', (req, res) => {
+
+    const id = parseInt(req.params.id) //SALVO IL PARAMETRO DINAMICO 'ID'
+    const showMoviesSql = 'SELECT * FROM movies WHERE id = ?' //SALVO LA PRIMA QUERY DA FARE AL DATABASE
+    const showReviewsSql = 'SELECT * FROM reviews WHERE movie_id = ?' // SALVO LA SECONDA QUERY DA FARE AL DATABASE
+    
+    connection.query(showMoviesSql, [id], (err, moviesResults) => { //ESEGUO LA PRIMA QUERY AL DATABASE
+        if (err) return res.status(500).json({error: 'Internal server error'}) //GESTISCO L'ERRORE LATO SERVER
+        if (moviesResults.length === 0) return res.status(404).json({error: 'Not found'}) //GESTISCO L'ERRORE LATO CLIENT
+        
+        const movie = moviesResults[0] //SALVO LA SINGOLA RISORSA [0] TROVATA IN UNA VARIABILE
+
+        connection.query(showReviewsSql, [id], (err, reviewsResults) => { //EFFETTUO LA SECONDA QUERY
+            
+          movie.reviews = reviewsResults  
+            
+          res.json(movie)
+        })
+    })
+})
+
 //MESSAGGIO DI LOG IN CASO DI CONNESSIONE
 app.listen(port, () => {
     console.log(`Sono in ascolto nel localhost alla porta ${port}`);
