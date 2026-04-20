08dc2c3d971da647fa846a5aebb82f217e320414
stefanosorrenti <stefanosorrenti01@gmail.com>
2026-04-20 12:50:59 +0200

    add index route

--- server.js
+++ server.js
@@ -2,7 +2,8 @@
 const express = require('express');
 const app = express()
 const port = 3000
-const connection = require('./database/db')
+const connection = require('./database/db');
+
 
 
 
@@ -12,6 +13,17 @@ app.get('/', (req, res) => {
     res.json('Benvenuto nel sever del film!')
 });
 
+//INDEX
+app.get('/movies', (req, res) => {
+    
+    const indexSql  = 'SELECT * FROM movies' //SALVO IN UNA VARIABILE LA QUERY DA FARE AL DB
+    connection.query(indexSql, (err, results) => { //EFFETTUO LA QUERY
+        
+        if (err) return res.status(500).json({error: 'Internal server error'}); //GESTISCO L'ERRORE LATO SERVER
+
+        res.json(results) //RESTITUISCO LA TABAELLA IN FORMATO JSON
+    })
+});
 
 //MESSAGGIO DI LOG IN CASO DI CONNESSIONE
 app.listen(port, () => {
